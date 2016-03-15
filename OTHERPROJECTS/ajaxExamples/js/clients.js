068942ae472e639d21f143c89def64011591eb74
jessnapes <jessicanapier@Jessicas-MacBook-Pro.local>
2016-03-15 11:56:50 -0400

    ugh

--- OTHERPROJECTS/ajaxExamples/js/clients.js
+++ OTHERPROJECTS/ajaxExamples/js/clients.js
@@ -1,21 +1,24 @@
 
-
+//get clients
 $("#getClients").on("click, function" () {
 
 
-
+//get json
 $.getJSON("http://jessnapes.github.io/OTHERPROJECTS/ajaxExamples/json/clients.json", function(data){
     
 $.each(data, function(index, item){
        $("#data").append(item.name);
+    
 })
     
-
+//this is to see if the clients is responding 
 
     alert("working");
 
 alert(data);
 console.dir(data);
 
+    
     })
+    
 })
