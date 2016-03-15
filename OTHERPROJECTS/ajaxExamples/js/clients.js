26c33c81ba5e45f25b30976a34fb43c95af1f55c
jessnapes <jessicanapier@Jessicas-MacBook-Pro.local>
2016-03-15 11:40:56 -0400

    sync

--- OTHERPROJECTS/ajaxExamples/js/clients.js
+++ OTHERPROJECTS/ajaxExamples/js/clients.js
@@ -1,8 +1,14 @@
+
+
 $("#getClients").on("click, function" () {
 
 
 
 $.getJSON("http://jessnapes.github.io/OTHERPROJECTS/ajaxExamples/json/clients.json", function(data){
+    
+$.each(data, function(index, item){
+       $("#data").append(item.name);
+})
     
 
 
