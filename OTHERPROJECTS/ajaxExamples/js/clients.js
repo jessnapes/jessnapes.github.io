8091549c9ff844841cedd430b8068ea0d5fad0ba
jessnapes <jessicanapier@Jessicas-MacBook-Pro.local>
2016-03-15 12:15:12 -0400

    JS clients tables working

--- OTHERPROJECTS/ajaxExamples/js/clients.js
+++ OTHERPROJECTS/ajaxExamples/js/clients.js
@@ -1,28 +1,28 @@
-$(document.ready(function(){
-    
-    
 
-//get clients
-$("#getClients").on("click, function" () {
+$(document).ready(function() {
 
+    $("#getClients").on("click", function() {
+        var url = "http://jessnapes.github.io/OTHERPROJECTS/ajaxExamples/jsonDatabase/clients.json";
+        $.getJSON("http://jessnapes.github.io/OTHERPROJECTS/ajaxExamples/jsonDatabase/clients.json", function(data) {
+            
+            var html = "<table class='table table-hover table-striped'>"
+        "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
+            
+            $.each(data, function(index, item){
+                
+        html += "<tr>" +
+            "<td>" + item.name + "</td>" +
+            "<td>" + item.email + "</td>" +
+            "<td>" + item.company + "</td>" +
+            "</tr>";
+            })
+            
+            html += "</table>";
+            $("#data").append(html);
 
-//get json
-$.getJSON("http://jessnapes.github.io/OTHERPROJECTS/ajaxExamples/json/clients.json", function(data){
-    
-    
-$.each(data, function(index, item){
-       $("#data").append(item.name);
-    
-})
-    
-//this is to see if the clients is responding 
+            //alert(data);
+            //console.dir(data);
 
-    alert("working");
-
-alert(data);
-console.dir(data);
-
- })   
-    })
-    
-})
+        });//getJSON
+    });//click
+});//function
