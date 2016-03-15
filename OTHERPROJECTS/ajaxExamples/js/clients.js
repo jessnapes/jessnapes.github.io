3ad8eafe8dbc1dce98d7dd8fa846956790c1b822
jessnapes <jessicanapier@Jessicas-MacBook-Pro.local>
2016-03-15 11:32:58 -0400

    CHANGEEE

--- OTHERPROJECTS/ajaxExamples/js/clients.js
+++ OTHERPROJECTS/ajaxExamples/js/clients.js
@@ -1,8 +1,15 @@
 $("#getClients").on("click, function" () {
 
 
-$.getJSON("http://jessnapes.github.io/OTHERPROJECTS/ajaxExamples/json/clients.json")
+
+$.getJSON("http://jessnapes.github.io/OTHERPROJECTS/ajaxExamples/json/clients.json", function(data){
+    
+
 
     alert("working");
 
+alert(data);
+console.dir(data);
+
+    })
 })
